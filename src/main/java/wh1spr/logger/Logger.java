36f86a4da1e759164760b95742f3c179b633502c
Wh1spr <wh1spr.code@gmail.com>
2018-02-14 23:56:35 +0100

    Added a dummy Logger
    will be implemented later, just needed it for a class in next commit

---
+++ src/main/java/wh1spr/logger/Logger.java
@@ -0,0 +1,25 @@
+package wh1spr.logger;
+
+public class Logger {
+
+	public Logger(String url) {
+		
+	}
+	
+	public void error(String msg) {
+		
+	}
+	
+	public void info(String msg) {
+		
+	}
+	
+	public void debug(String msg) {
+		
+	}
+	
+	public void warning(String msg) {
+		
+	}
+	
+}
---
+++ src/main/java/wh1spr/logger/LoggerCache.java
@@ -0,0 +1,5 @@
+package wh1spr.logger;
+
+public class LoggerCache {
+
+}
