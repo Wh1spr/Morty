903e960ad76d5684e79d5f6d64a87ab6f7481b36
Wh1spr <wh1spr.code@gmail.com>
2018-05-26 11:17:09 +0200

    Added embed to ecosetup

--- src/main/java/wh1spr/bot/commands/economy/settings/EcoSetupCommand.java
+++ src/main/java/wh1spr/bot/commands/economy/settings/EcoSetupCommand.java
@@ -1,13 +1,16 @@
 package wh1spr.bot.commands.economy.settings;
 
+import java.awt.Color;
 import java.util.List;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import wh1spr.bot.Main;
+import wh1spr.bot.Tools;
 import wh1spr.bot.command.Command;
 import wh1spr.bot.commands.economy.util.EconomyStatus;
 import wh1spr.bot.database.EcoInfo;
@@ -32,32 +35,57 @@ public class EcoSetupCommand extends Command {
 		
 		boolean hadEco = EconomyStatus.hasEconomy(guild);
 		
-		final double start;
+		double start;
 		double daily = 0.0;
 		try {
 			start = Double.parseDouble(args.get(0));
 			daily = Double.parseDouble(args.get(1));
+			
+			start = Tools.round(start);
+			daily = Tools.round(daily);
 		} catch(NumberFormatException e) {
 			failure(message);
 			channel.sendMessage("Make sure you use '.' as a decimal separator, and the order of your arguments is correct.").queue();
 			return;
 		}
+		String majSing = args.get(2);
+		String majMult = args.get(3);
+		String minSing = args.get(4);
+		String minMult = args.get(5);
+		String s = "**Currency names**\n\n• Major Singular\n	*%s*"
+				+ "\n\n• Major Plural\n	*%s*"
+				+ "\n\n• Minor Singular\n	*%s*"
+				+ "\n\n• Minor Plural\n	*%s*"
+				+ "\n\n• Daily Balance\n	*%.2f*"
+				+ "\n\n• Start Balance\n	*%.2f*";
 		
-		Main.getBots().get(0).getDb().setupEconomy(guild.getId(), args.get(2), args.get(3), args.get(4), args.get(5), start, daily);
+		if (majSing.toUpperCase().equals("DEF")) majSing = "Dollar";
+		if (majMult.toUpperCase().equals("DEF")) majMult = "Dollars";
+		if (minSing.toUpperCase().equals("DEF")) minSing = "Cent";
+		if (majSing.toUpperCase().equals("DEF")) minMult = "Cents";
 		
+		s = String.format(s, majSing, majMult, minSing, majSing, daily, start);
+		
+		if(!Main.getBots().get(0).getDb().setupEconomy(guild.getId(), majSing, majMult, minSing, minMult, start, daily)){
+			warning(message);
+			return;
+		}
+		
+		final double startcopy = start;
 		if (!hadEco) {
 			//setup balances
 			guild.getMembers().forEach(el-> {
 				if (!el.getUser().isBot())
-					EconomyStatus.createBalance(el, start);
+					EconomyStatus.createBalance(el, startcopy);
 			});
 			EconomyStatus.updateBalances();
 		}
-		
-		EcoInfo e = EconomyStatus.getGuildInfo(guild);
 		// embed to show it went through
-		success(message);
+		EmbedBuilder e = new EmbedBuilder().setColor(Color.GREEN)
+				.setTitle("**Economy Setup Complete!**")
+				.setDescription(s);
 		
+		channel.sendMessage(e.build()).queue();
 	}
 	
 	@Override
