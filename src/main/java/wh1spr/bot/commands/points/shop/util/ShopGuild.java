e4f88b7174047c544324955faa0ff3c6a251f448
Wh1spr <wh1spr.code@gmail.com>
2018-09-25 12:27:31 +0200

    added bought() to ShopGuild

--- src/main/java/wh1spr/bot/commands/points/shop/util/ShopGuild.java
+++ src/main/java/wh1spr/bot/commands/points/shop/util/ShopGuild.java
@@ -3,11 +3,13 @@ package wh1spr.bot.commands.points.shop.util;
 import static com.mongodb.client.model.Updates.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.model.PushOptions;
 
 import net.dv8tion.jda.core.entities.Guild;
 import wh1spr.bot.commands.points.util.PointTypeManager;
@@ -39,11 +41,14 @@ public class ShopGuild extends MongoGuild {
 		return (List<Document>)this.getDoc().get(tm.getModuleName(), Document.class).get(tm.getTypeName() + "shop");
 	}
 	
+	private List<ShopItem> list = null;
 	public List<ShopItem> getItems() {
+		if (this.list!=null) return this.list;
 		List<ShopItem> list = new ArrayList<ShopItem>();
 		getShopArray().forEach(el -> {
 			list.add(new ShopItem(el));
 		});
+		this.list = list;
 		return list;
 	}
 
@@ -57,6 +62,7 @@ public class ShopGuild extends MongoGuild {
 	}
 	
 	public boolean addItem(ShopItem item) {
+		list = null;
 		if (getShopArray().size() < 10) { //limit to 10 items
 			this.bsonUpdates(push(tm.getModuleName() + "." + tm.getTypeName() + "shop", item.getDocument()));
 			return true;
@@ -65,9 +71,31 @@ public class ShopGuild extends MongoGuild {
 		}
 	}
 	
+	public boolean bought(ShopItem item, int amount) { //amount in item is in shop
+		if (getItems().indexOf(item)==-1) throw new IllegalArgumentException("This item does not exist in this store.");
+		if (item.getAmount() < amount) {
+			return false;
+		} else {
+			ShopItem clone = item.clone();
+			if (item.getAmount()!=-1)
+				clone.setAmount(item.getAmount() - amount);
+			int index = getItems().indexOf(item);
+			removeItem(index);
+			list=null;
+			this.bsonUpdates(pushEach(tm.getModuleName() + "." + tm.getTypeName() + "shop", Arrays.asList(clone.getDocument()), new PushOptions().position(index)));
+			return true;
+		}
+	}
+	
 	public void removeItem(int index) {
 		if (getShopArray().size() <= index) return;
 		ShopItem toDel = getItems().get(index);
+		list=null;
 		this.bsonUpdates(pull(tm.getModuleName() + "." + tm.getTypeName() + "shop", toDel.getDocument()));
+	}
+	
+	public void removeItem(ShopItem item) {
+		list=null;
+		this.bsonUpdates(pull(tm.getModuleName() + "." + tm.getTypeName() + "shop", item.getDocument()));
 	}
 }
