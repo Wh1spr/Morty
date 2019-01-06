fab5ef691807dadd4251181349ca2f46a919c829
Wh1spr <wh1spr.code@gmail.com>
2019-01-06 10:21:52 +0100

    Made ids be Long instead of String
    This basically forfeits the database i have now

--- src/main/java/wh1spr/bot/mongodb/BasicUpdateMongoItem.java
+++ src/main/java/wh1spr/bot/mongodb/BasicUpdateMongoItem.java
@@ -2,9 +2,14 @@ package wh1spr.bot.mongodb;
 
 public abstract class BasicUpdateMongoItem extends BasicMongoItem {
 
-	protected BasicUpdateMongoItem(String collection, String id) {
+	protected BasicUpdateMongoItem(String collection, long id) {
 		super(collection, id);
 	}
 
+	@Deprecated
+	protected BasicUpdateMongoItem(String collection, String id) {
+		super(collection, id);
+	}
+	
 	protected abstract boolean update();
 }
